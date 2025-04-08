4ee216917d81ded35013e3c69aeda7e8a5c5c03b
momotheduckyy <154822774+momotheduckyy@users.noreply.github.com>
2025-04-07 21:21:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,75 @@ def MED(S, T):
             return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
-def fast_MED(S, T, MED={}):
+def fast_MED(S, T, MED=None):
     # TODO -  implement top-down memoization
-    pass
+    f MED is None:
+        MED = {}
 
-def fast_align_MED(S, T, MED={}):
+    if (S, T) in MED:
+        return MED[(S, T)]
+
+    if len(S) == 0:
+        MED[(S, T)] = len(T)
+        return len(T)
+    if len(T) == 0:
+        MED[(S, T)] = len(S)
+        return len(S)
+
+    if S[0] == T[0]:
+        result = fast_MED(S[1:], T[1:], MED)
+    else:
+        insert = fast_MED(S, T[1:], MED)
+        delete = fast_MED(S[1:], T, MED)
+        substitute = fast_MED(S[1:], T[1:], MED)
+        result = 1 + min(insert, delete, substitute)
+
+    MED[(S, T)] = result
+    return result
+
+def fast_align_MED(S, T, MED=None):
     # TODO - keep track of alignment
-    pass
+    if MED is None:
+        MED = {}
+
+    if (S, T) in MED:
+        return MED[(S, T)]
+
+    # Base cases
+    if len(S) == 0:
+        aligned_S = '-' * len(T)
+        aligned_T = T
+        result = (len(T), aligned_S, aligned_T)
+        MED[(S, T)] = result
+        return result
+
+    if len(T) == 0:
+        aligned_S = S
+        aligned_T = '-' * len(S)
+        result = (len(S), aligned_S, aligned_T)
+        MED[(S, T)] = result
+        return result
+
+    if S[0] == T[0]:
+        dist, rest_S, rest_T = fast_align_MED(S[1:], T[1:], MED)
+        result = (dist, S[0] + rest_S, T[0] + rest_T)
+        MED[(S, T)] = result
+        return result
+
+    # Insertion
+    insert_dist, insert_S, insert_T = fast_align_MED(S, T[1:], MED)
+    insert = (1 + insert_dist, '-' + insert_S, T[0] + insert_T)
+
+    # Deletion
+    delete_dist, delete_S, delete_T = fast_align_MED(S[1:], T, MED)
+    delete = (1 + delete_dist, S[0] + delete_S, '-' + delete_T)
+
+    # Substitution
+    subst_dist, subst_S, subst_T = fast_align_MED(S[1:], T[1:], MED)
+    subst = (1 + subst_dist, T[0] + subst_S, T[0] + subst_T)
+
+    # Choose best
+    result = min(insert, delete, subst, key=lambda x: x[0])
+    MED[(S, T)] = result
+    return result
 
